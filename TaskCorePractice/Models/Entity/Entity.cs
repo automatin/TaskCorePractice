22fc212a60c889591f7492f1d38ebbe2208bf2c8
Ramiro <ramiro_lfc@hotmail.com>
2019-04-03 10:54:28 -0300

    Add new Entitys to the project

---
+++ TaskCorePractice/Models/Entity/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskCorePractice.Models.Entity
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ TaskCorePractice/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskCorePractice.Models.Entity;
+
+namespace TaskCorePractice.Models
+{
+    public class User : TaskCorePractice.Models.Entity.Entity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string  UserName { get; set; }
+        public string Password { get; set; }
+        public List<UserTask> TaskList{ get; set; }
+
+        public User()
+        {
+            TaskList = new List<UserTask>();        
+        }
+    }
+}
